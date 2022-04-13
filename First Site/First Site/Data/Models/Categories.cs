9351e0170c611be3645edc71188a2e8e642c52ce
K11MMILK <103606207+K11MMILK@users.noreply.github.com>
2022-04-14 00:12:04 +0300

    Update Categories.cs

--- First Site/First Site/Data/Models/Categories.cs
+++ First Site/First Site/Data/Models/Categories.cs
@@ -10,6 +10,5 @@ namespace First_Site.Data.Models
         public int id { set; get; }
         public string categoryName { set; get; }
         public List<Page> pages{ set; get; }
-        //ввв
     }
 }
