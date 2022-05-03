b466351ff7bdc6f75e7ba50955e565a97afdde5c
helpMePlease81 <102649703+helpMePlease81@users.noreply.github.com>
2022-05-03 19:38:06 +0300

    Add files via upload

---
+++ First site V2/Storage/Entity/Music.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace First_site_V2.Storage.Entity
+{
+    public class Music
+    {
+
+    }
+}
